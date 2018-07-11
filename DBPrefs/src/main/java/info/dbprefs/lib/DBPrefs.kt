ddca9869f3aa5b9a8b026ada85808fb4badb9f6c
Hannes Achleitner <hannes.software@gmx.at>
2018-07-11 06:11:01 +0200

    add functions support String param

--- DBPrefs/src/main/java/info/dbprefs/lib/DBPrefs.kt
+++ DBPrefs/src/main/java/info/dbprefs/lib/DBPrefs.kt
@@ -1,5 +1,6 @@
 package info.dbprefs.lib
 
+import android.annotation.SuppressLint
 import android.arch.persistence.room.Room
 import android.content.Context
 import android.provider.Settings
@@ -23,17 +24,32 @@ class DBPrefs {
     }
 
     fun <T> put(key: ConfigKey, value: T): Boolean {
-        return putSerialized<Any>(key, mParse.toJson(value))
+        return putSerialized(key, mParse.toJson(value))
+    }
+
+    fun <T> put(key: String, value: T): Boolean {
+        return putSerialized(key, mParse.toJson(value))
     }
 
     fun <T> put(key: ConfigKey, value: List<T>): Boolean {
-        return putSerialized<Any>(key, mParse.toJson(value))
+        return putSerialized(key, mParse.toJson(value))
     }
 
-    private fun <T> putSerialized(key: ConfigKey, value: String?): Boolean {
+    private fun putSerialized(key: ConfigKey, value: String?): Boolean {
         if (value != null) {
             val pref = PreferenceRoom()
             pref.key = key.toString()
+            pref.value = value
+            appDatabase.preferenceDao().insert(pref)
+        }
+        return true
+    }
+
+    @Deprecated(message = "Try to avoid using a String")
+    private fun putSerialized(key: String, value: String?): Boolean {
+        if (value != null) {
+            val pref = PreferenceRoom()
+            pref.key = key
             pref.value = value
             appDatabase.preferenceDao().insert(pref)
         }
@@ -52,8 +68,41 @@ class DBPrefs {
         return null
     }
 
+    fun <T> get(key: String, type: Type): T? {
+        val returningClass: T?
+        val decodedText = getSerialized(key) ?: return null
+        try {
+            returningClass = mParse.fromJson<T>(decodedText, type)
+            return returningClass
+        } catch (e: Exception) {
+            Log.e(e.message, "Exception for class $type decoded Text: $decodedText")
+        }
+        return null
+    }
+
+    fun <T> get(key: String, type: Type, default : T): T? {
+        val returningClass: T?
+        val decodedText = getSerialized(key) ?: return null
+        try {
+            returningClass = mParse.fromJson<T>(decodedText, type)
+            return returningClass
+        } catch (e: Exception) {
+            Log.e(e.message, "Exception for class $type decoded Text: $decodedText")
+        }
+        return default
+    }
+
     private fun getSerialized(key: ConfigKey): String? {
         val value = appDatabase.preferenceDao().getValue(key.toString())
+        return if (value == null)
+            return null
+        else
+            return value.value
+    }
+
+    @Deprecated(message = "Try to avoid using a String")
+    private fun getSerialized(key: String): String? {
+        val value = appDatabase.preferenceDao().getValue(key)
         return if (value == null)
             return null
         else
@@ -88,6 +137,10 @@ class DBPrefs {
         appDatabase.preferenceDao().deleteByKey(key.toString())
     }
 
+    fun remove(key: String) {
+        appDatabase.preferenceDao().deleteByKey(key)
+    }
+
     fun clearAll() {
         appDatabase.clearAllTables()
     }
@@ -100,10 +153,31 @@ class DBPrefs {
         return appDatabase.preferenceDao().countKey(key.toString()) == 1
     }
 
+    fun contains(key: String): Boolean {
+        return appDatabase.preferenceDao().countKey(key) == 1
+    }
+
+    fun count(): Int {
+        return appDatabase.preferenceDao().all.size
+    }
+
+    fun clearAllExcept(vararg keys: String) {
+        appDatabase.preferenceDao().all.forEach {
+            if (!keys.contains(it.key)) {
+                appDatabase.preferenceDao().deleteByKey(it.key)
+            }
+        }
+    }
+
+    fun getAll() : List<String> {
+        return appDatabase.preferenceDao().all.map { it.key }
+    }
+
     @Suppress("unused")
     companion object {
         lateinit var appDatabase: AppDatabase
 
+        @SuppressLint("HardwareIds")
         fun init(context: Context) {
             init(context, Settings.Secure.getString(context.getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID))
         }
