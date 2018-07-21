443cb11b98fce6f2ded62db8ff09085c64d557fc
Hannes Achleitner <hannes.software@gmx.at>
2018-07-21 14:56:19 +0200

    update version

--- DBPrefs/src/androidTest/java/info/dbprefs/androidtests/DBPReferencesTest.kt
+++ DBPrefs/src/androidTest/java/info/dbprefs/androidtests/DBPReferencesTest.kt
@@ -195,7 +195,7 @@ class DBPReferencesTest {
         for (item: Int in 1..2000) {
             dbPrefs.put(TestConfigKeys.KEY_STRING, "value" + item)
         }
-        Assert.assertTrue((System.currentTimeMillis() - start) < 20000)  // ARM needs more time, x86 works with 6000
+        Assert.assertTrue((System.currentTimeMillis() - start) < 20000)  // ARM needs more time, x86 works with 9000
 
         start = System.currentTimeMillis()
         for (item: Int in 1..2000) {
