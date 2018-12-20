8becec2e7b1d977bb1e7c2a61bd7c9f21a5e4a3b
Hannes Achleitner <hannes.software@gmx.at>
2018-12-20 19:13:08 +0100

    update libs

--- sample/src/main/java/info/dbprefs/sample/MainActivity.kt
+++ sample/src/main/java/info/dbprefs/sample/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.reflect.TypeToken
 import info.dbprefs.lib.DbPreferences
-import info.dbprefs.sample.R.id.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
